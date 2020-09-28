328ade5c481e233f641cf69d81a31b7384c60cf2
Milan Sai <Milan.Sai@team.telstra.com>
2020-09-29 01:49:37 +0530

    Updates

---
+++ precache-manifest.f11be12e600b05c376bcd4869e5caaf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f9e492f4a6af5e9142c0484e7b050f",
+    "url": "/ONLINE_EXAM_PORTA/index.html"
+  },
+  {
+    "revision": "a63951a720eaf4407160",
+    "url": "/ONLINE_EXAM_PORTA/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6380532d0d42361e54f7",
+    "url": "/ONLINE_EXAM_PORTA/static/js/2.99d075a1.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/ONLINE_EXAM_PORTA/static/js/2.99d075a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a63951a720eaf4407160",
+    "url": "/ONLINE_EXAM_PORTA/static/js/main.0c84a67f.chunk.js"
+  },
+  {
+    "revision": "80e78e55e4882886e26b",
+    "url": "/ONLINE_EXAM_PORTA/static/js/runtime-main.a8a4caf8.js"
+  },
+  {
+    "revision": "09eb96bfd7c28c3d6e4e24ee2b2b6705",
+    "url": "/ONLINE_EXAM_PORTA/static/media/logo.09eb96bf.svg"
+  }
+]);
